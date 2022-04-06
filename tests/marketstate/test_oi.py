2b4950066d1d42f6d6ec3313f8a1f57360a38278
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2022-04-06 08:48:53 -0400

    Add revert test when no market for oi

--- tests/marketstate/test_oi.py
+++ tests/marketstate/test_oi.py
@@ -1,6 +1,6 @@
 import pytest
 from pytest import approx
-from brownie import chain
+from brownie import chain, reverts
 from decimal import Decimal
 from math import exp, sqrt
 
@@ -99,3 +99,8 @@ def test_oi_is_zero_when_no_positions(market_state, feed):
     actual_oi_long, actual_oi_short = market_state.oi(feed)
     assert actual_oi_long == 0
     assert actual_oi_short == 0
+
+
+def test_oi_reverts_when_no_market(market_state, rando):
+    with reverts("OVLV1:!market"):
+        _, _ = market_state.oi(rando)
