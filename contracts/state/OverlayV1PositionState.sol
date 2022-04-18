dfcc4ce4ab35432db15632bfd46f3de02a7307d7
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2022-04-18 09:19:24 -0400

    Fix OverlayV1PositionState for internal views

--- contracts/state/OverlayV1PositionState.sol
+++ contracts/state/OverlayV1PositionState.sol
@@ -46,35 +46,29 @@ abstract contract OverlayV1PositionState is
         });
     }
 
-    /// @notice Gets the position from the Overlay market associated with
-    /// @notice the given feed for the given position owner and position id
-    // TODO: test
-    function position(
-        address feed,
-        address owner,
-        uint256 id
-    ) external view returns (Position.Info memory position_) {
-        IOverlayV1Market market = _getMarket(feed);
-        position_ = _getPosition(market, owner, id);
+    /// @dev current debt owed by individual position
+    function _debt(Position.Info memory position) internal view returns (uint256 debt_) {
+        // assume entire position value such that fraction = ONE
+        uint256 fraction = FixedPoint.ONE;
+
+        // return the debt
+        debt_ = position.debtCurrent(fraction);
     }
 
-    // TODO: pos views: value, pnl, notionalWithPnl, collateral, liquidatable
-    // TODO: getAccountLiquidity() equivalent from Comptroller (PnL + value)
+    function _cost(Position.Info memory position) internal view returns (uint256 cost_) {
+        // assume entire position value such that fraction = ONE
+        uint256 fraction = FixedPoint.ONE;
 
-    /// @notice Gets the current open interest of the position on the Overlay
-    /// @notice market associated with the given feed address for the given
-    /// @notice position owner, id
-    /// @return oi_ as the current open interest occupied by the position
-    // TODO: test
-    function oi(
-        address feed,
-        address owner,
-        uint256 id
-    ) external view returns (uint256 oi_) {
-        IOverlayV1Market market = _getMarket(feed);
-        Oracle.Data memory data = _getOracleData(feed);
-        Position.Info memory position = _getPosition(market, owner, id);
+        // return the cost
+        cost_ = position.cost(fraction);
+    }
 
+    /// @dev current oi occupied by individual position
+    function _oi(IOverlayV1Market market, Position.Info memory position)
+        internal
+        view
+        returns (uint256 oi_)
+    {
         // assume entire position value such that fraction = ONE
         uint256 fraction = FixedPoint.ONE;
 
@@ -92,65 +86,12 @@ abstract contract OverlayV1PositionState is
         oi_ = position.oiCurrent(fraction, oiTotalOnSide, oiTotalSharesOnSide);
     }
 
-    /// @notice Gets the current debt of the position on the Overlay
-    /// @notice market associated with the given feed address for the given
-    /// @notice position owner, id
-    /// @return debt_ as the current debt taken on by the position
-    // TODO: test
-    function debt(
-        address feed,
-        address owner,
-        uint256 id
-    ) external view returns (uint256 debt_) {
-        IOverlayV1Market market = _getMarket(feed);
-        Oracle.Data memory data = _getOracleData(feed);
-        Position.Info memory position = _getPosition(market, owner, id);
-
-        // assume entire position value such that fraction = ONE
-        uint256 fraction = FixedPoint.ONE;
-
-        // return the debt
-        debt_ = position.debtCurrent(fraction);
-    }
-
-    /// @notice Gets the current cost of the position on the Overlay
-    /// @notice market associated with the given feed address for the given
-    /// @notice position owner, id
-    /// @return cost_ as the cost to build the position
-    // TODO: test
-    function cost(
-        address feed,
-        address owner,
-        uint256 id
-    ) external view returns (uint256 cost_) {
-        IOverlayV1Market market = _getMarket(feed);
-        Oracle.Data memory data = _getOracleData(feed);
-        Position.Info memory position = _getPosition(market, owner, id);
-
-        // assume entire position value such that fraction = ONE
-        uint256 fraction = FixedPoint.ONE;
-
-        // return the cost
-        cost_ = position.cost(fraction);
-    }
-
-    /// @notice Gets the current collateral backing the position on the
-    /// @notice Overlay market associated with the given feed address
-    /// @notice for the given position owner, id
-    /// @dev N(t) = Q * (OI(t) / OI(0)) - D; where Q = notional at build,
-    /// @dev OI(t) = current open interest, OI(0) = open interest at build,
-    /// @dev D = debt at build
-    /// @return collateral_ as the current collateral backing the position
-    // TODO: test
-    function collateral(
-        address feed,
-        address owner,
-        uint256 id
-    ) external view returns (uint256 collateral_) {
-        IOverlayV1Market market = _getMarket(feed);
-        Oracle.Data memory data = _getOracleData(feed);
-        Position.Info memory position = _getPosition(market, owner, id);
-
+    /// @dev current collateral backing the individual position
+    function _collateral(IOverlayV1Market market, Position.Info memory position)
+        internal
+        view
+        returns (uint256 collateral_)
+    {
         // assume entire position value such that fraction = ONE
         uint256 fraction = FixedPoint.ONE;
 
@@ -176,20 +117,12 @@ abstract contract OverlayV1PositionState is
         collateral_ = q.mulUp(oiCurrent).divUp(oiInitial).subFloor(d);
     }
 
-    /// @notice Gets the current value of the position on the Overlay market
-    /// @notice associated with the given feed address for the given
-    /// @notice position owner, id
-    /// @return value_ as the current value of the position
-    // TODO: test
-    function value(
-        address feed,
-        address owner,
-        uint256 id
-    ) external view returns (uint256 value_) {
-        IOverlayV1Market market = _getMarket(feed);
-        Oracle.Data memory data = _getOracleData(feed);
-        Position.Info memory position = _getPosition(market, owner, id);
-
+    /// @dev current value of the individual position
+    function _value(IOverlayV1Market market, Oracle.Data memory data, Position.Info memory position)
+        internal
+        view
+        returns (uint256 value_)
+    {
         // assume entire position value such that fraction = ONE
         uint256 fraction = FixedPoint.ONE;
 
@@ -208,10 +141,9 @@ abstract contract OverlayV1PositionState is
 
         // current price is price position would receive if unwound
         // longs get the bid on unwind, shorts get the ask
-        uint256 volume = _fractionOfCapOi(market, data, oi);
         uint256 currentPrice = position.isLong
-            ? _bid(market, data, volume)
-            : _ask(market, data, volume);
+            ? _bid(market, data, _fractionOfCapOi(market, data, oi))
+            : _ask(market, data, _fractionOfCapOi(market, data, oi));
 
         // get cap payoff from risk params
         uint256 capPayoff = market.params(uint256(Risk.Parameters.CapPayoff));
@@ -226,20 +158,12 @@ abstract contract OverlayV1PositionState is
         );
     }
 
-    /// @notice Gets the current notional of the position on the Overlay market
-    /// @notice associated with the given feed address for the given
-    /// @notice position owner, id (accounts for PnL)
-    /// @return notional_ as the current notional of the position
-    // TODO: test
-    function notional(
-        address feed,
-        address owner,
-        uint256 id
-    ) external view returns (uint256 notional_) {
-        IOverlayV1Market market = _getMarket(feed);
-        Oracle.Data memory data = _getOracleData(feed);
-        Position.Info memory position = _getPosition(market, owner, id);
-
+    /// @dev current notional (including PnL) of the individual position
+    function _notional(IOverlayV1Market market, Oracle.Data memory data, Position.Info memory position)
+        internal
+        view
+        returns (uint256 notional_)
+    {
         // assume entire position value such that fraction = ONE
         uint256 fraction = FixedPoint.ONE;
 
@@ -258,10 +182,9 @@ abstract contract OverlayV1PositionState is
 
         // current price is price position would receive if unwound
         // longs get the bid on unwind, shorts get the ask
-        uint256 volume = _fractionOfCapOi(market, data, oi);
         uint256 currentPrice = position.isLong
-            ? _bid(market, data, volume)
-            : _ask(market, data, volume);
+            ? _bid(market, data, _fractionOfCapOi(market, data, oi))
+            : _ask(market, data, _fractionOfCapOi(market, data, oi));
 
         // get cap payoff from risk params
         uint256 capPayoff = market.params(uint256(Risk.Parameters.CapPayoff));
@@ -274,5 +197,115 @@ abstract contract OverlayV1PositionState is
             currentPrice,
             capPayoff
         );
+    }
+
+    /// @notice Gets the position from the Overlay market associated with
+    /// @notice the given feed for the given position owner and position id
+    // TODO: test
+    function position(
+        address feed,
+        address owner,
+        uint256 id
+    ) external view returns (Position.Info memory position_) {
+        IOverlayV1Market market = _getMarket(feed);
+        position_ = _getPosition(market, owner, id);
+    }
+
+    // TODO: pos views: value, pnl, notionalWithPnl, collateral, liquidatable
+    // TODO: getAccountLiquidity() equivalent from Comptroller (PnL + value)
+
+    /// @notice Gets the current debt of the position on the Overlay
+    /// @notice market associated with the given feed address for the given
+    /// @notice position owner, id
+    /// @return debt_ as the current debt taken on by the position
+    // TODO: test
+    function debt(
+        address feed,
+        address owner,
+        uint256 id
+    ) external view returns (uint256 debt_) {
+        IOverlayV1Market market = _getMarket(feed);
+        Position.Info memory position = _getPosition(market, owner, id);
+        debt_ = _debt(position);
+    }
+
+    /// @notice Gets the current cost of the position on the Overlay
+    /// @notice market associated with the given feed address for the given
+    /// @notice position owner, id
+    /// @return cost_ as the cost to build the position
+    // TODO: test
+    function cost(
+        address feed,
+        address owner,
+        uint256 id
+    ) external view returns (uint256 cost_) {
+        IOverlayV1Market market = _getMarket(feed);
+        Position.Info memory position = _getPosition(market, owner, id);
+        cost_ = _cost(position);
+    }
+
+    /// @notice Gets the current open interest of the position on the Overlay
+    /// @notice market associated with the given feed address for the given
+    /// @notice position owner, id
+    /// @return oi_ as the current open interest occupied by the position
+    // TODO: test
+    function oi(
+        address feed,
+        address owner,
+        uint256 id
+    ) external view returns (uint256 oi_) {
+        IOverlayV1Market market = _getMarket(feed);
+        Position.Info memory position = _getPosition(market, owner, id);
+        oi_ = _oi(market, position);
+    }
+
+    /// @notice Gets the current collateral backing the position on the
+    /// @notice Overlay market associated with the given feed address
+    /// @notice for the given position owner, id
+    /// @dev N(t) = Q * (OI(t) / OI(0)) - D; where Q = notional at build,
+    /// @dev OI(t) = current open interest, OI(0) = open interest at build,
+    /// @dev D = debt at build
+    /// @return collateral_ as the current collateral backing the position
+    // TODO: test
+    function collateral(
+        address feed,
+        address owner,
+        uint256 id
+    ) external view returns (uint256 collateral_) {
+        IOverlayV1Market market = _getMarket(feed);
+        Position.Info memory position = _getPosition(market, owner, id);
+        collateral_ = _collateral(market, position);
+    }
+
+    /// @notice Gets the current value of the position on the Overlay market
+    /// @notice associated with the given feed address for the given
+    /// @notice position owner, id
+    /// @return value_ as the current value of the position
+    // TODO: test
+    function value(
+        address feed,
+        address owner,
+        uint256 id
+    ) external view returns (uint256 value_) {
+        IOverlayV1Market market = _getMarket(feed);
+        Oracle.Data memory data = _getOracleData(feed);
+        Position.Info memory position = _getPosition(market, owner, id);
+        value_ = _value(market, data, position);
+    }
+
+    /// @notice Gets the current notional of the position on the Overlay market
+    /// @notice associated with the given feed address for the given
+    /// @notice position owner, id (accounts for PnL)
+    /// @return notional_ as the current notional of the position
+    // TODO: test
+    function notional(
+        address feed,
+        address owner,
+        uint256 id
+    ) external view returns (uint256 notional_) {
+        IOverlayV1Market market = _getMarket(feed);
+        Oracle.Data memory data = _getOracleData(feed);
+        Position.Info memory position = _getPosition(market, owner, id);
+        notional_ = _notional(market, data, position);
     }
 }
