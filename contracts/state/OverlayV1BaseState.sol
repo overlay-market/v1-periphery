bf751fe1890191ddaf64a801f4551d5a6eae4324
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2022-04-18 08:35:44 -0400

    Move _getPosition to position state contract

--- contracts/state/OverlayV1BaseState.sol
+++ contracts/state/OverlayV1BaseState.sol
@@ -30,34 +30,6 @@ abstract contract OverlayV1BaseState {
         data_ = IOverlayV1Feed(feed).latest();
     }
 
-    /// @notice Gets the position from the given market for the
-    /// @notice position owner and position id
-    function _getPosition(
-        IOverlayV1Market market,
-        address owner,
-        uint256 id
-    ) internal view returns (Position.Info memory position_) {
-        bytes32 key = keccak256(abi.encodePacked(owner, id));
-        (
-            uint96 notional,
-            uint96 debt,
-            uint48 entryToMidRatio,
-            bool isLong,
-            bool liquidated,
-            uint256 oiShares
-        ) = market.positions(key);
-
-        // assemble the position info struct
-        position_ = Position.Info({
-            notional: notional,
-            debt: debt,
-            entryToMidRatio: entryToMidRatio,
-            isLong: isLong,
-            liquidated: liquidated,
-            oiShares: oiShares
-        });
-    }
-
     /// @notice Gets the Overlay market address for the given feed
     /// @dev reverts if market doesn't exist
     // TODO: test
@@ -69,17 +41,5 @@ abstract contract OverlayV1BaseState {
     // TODO: test
     function data(address feed) external view returns (Oracle.Data memory data_) {
         data_ = _getOracleData(feed);
-    }
-
-    /// @notice Gets the position from the Overlay market associated with
-    /// @notice the given feed for the given position owner and position id
-    // TODO: test
-    function position(
-        address feed,
-        address owner,
-        uint256 id
-    ) external view returns (Position.Info memory position_) {
-        IOverlayV1Market market = _getMarket(feed);
-        position_ = _getPosition(market, owner, id);
     }
 }
--- contracts/state/OverlayV1PositionState.sol
+++ contracts/state/OverlayV1PositionState.sol
@@ -18,6 +18,46 @@ abstract contract OverlayV1PositionState is
     using FixedPoint for uint256;
     using Position for Position.Info;
 
+    /// @notice Gets the position from the given market for the
+    /// @notice position owner and position id
+    function _getPosition(
+        IOverlayV1Market market,
+        address owner,
+        uint256 id
+    ) internal view returns (Position.Info memory position_) {
+        bytes32 key = keccak256(abi.encodePacked(owner, id));
+        (
+            uint96 notional,
+            uint96 debt,
+            uint48 entryToMidRatio,
+            bool isLong,
+            bool liquidated,
+            uint256 oiShares
+        ) = market.positions(key);
+
+        // assemble the position info struct
+        position_ = Position.Info({
+            notional: notional,
+            debt: debt,
+            entryToMidRatio: entryToMidRatio,
+            isLong: isLong,
+            liquidated: liquidated,
+            oiShares: oiShares
+        });
+    }
+
+    /// @notice Gets the position from the Overlay market associated with
+    /// @notice the given feed for the given position owner and position id
+    // TODO: test
+    function position(
+        address feed,
+        address owner,
+        uint256 id
+    ) external view returns (Position.Info memory position_) {
+        IOverlayV1Market market = _getMarket(feed);
+        position_ = _getPosition(market, owner, id);
+    }
+
     // TODO: pos views: value, pnl, notionalWithPnl, collateral, liquidatable
     // TODO: getAccountLiquidity() equivalent from Comptroller (PnL + value)
 
