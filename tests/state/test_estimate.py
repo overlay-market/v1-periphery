76f6459e20ca684d69bc54b81f7c084355539f1d
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2022-05-06 15:05:00 -0400

    Add test for liq price estimate

--- tests/state/test_estimate.py
+++ tests/state/test_estimate.py
@@ -1,3 +1,4 @@
+from pytest import approx
 from brownie.test import given, strategy
 from decimal import Decimal
 
@@ -122,3 +123,50 @@ def test_maintenance_margin_estimate(state, market, feed, alice, ovl, is_long):
     actual = state.maintenanceMarginEstimate(
         market, collateral, leverage, is_long)
     assert expect == actual
+
+
+@given(is_long=strategy('bool'))
+def test_liquidation_price_estimate(state, market, feed, alice, ovl, is_long):
+    # alice build params
+    collateral = 20000000000000000000  # 20
+    leverage = 3000000000000000000  # 3
+
+    # calculate the notional
+    notional = int(Decimal(collateral)
+                   * Decimal(leverage) / Decimal(1e18))
+
+    # get the mid price
+    # NOTE: state.mid() tested in test_price.py
+    mid_price = state.mid(market)
+
+    # calculate the oi
+    # NOTE: state.fractionOfCapOi() tested in test_oi.py
+    oi = int(Decimal(notional) * Decimal(1e18) / Decimal(mid_price))
+    fraction_oi = state.fractionOfCapOi(market, oi)
+
+    # get the entry price
+    # NOTE: state.bid() and state.ask() tested in test_price.py
+    entry_price = state.ask(market, fraction_oi) if is_long else state.bid(
+        market, fraction_oi)
+
+    # calculate expected values
+    maintenance_margin_fraction = market.params(
+        RiskParameter.MAINTENANCE_MARGIN_FRACTION.value)
+    maintenance_margin = Decimal(
+        notional) * Decimal(maintenance_margin_fraction) / Decimal(1e18)
+    liq_fee_rate = market.params(
+        RiskParameter.LIQUIDATION_FEE_RATE.value)
+
+    # calculate liquidation price
+    dp = (Decimal(collateral) - Decimal(maintenance_margin) * Decimal(1e18)
+          / (Decimal(1e18) - Decimal(liq_fee_rate))) / Decimal(oi)
+    dp *= Decimal(1e18)
+
+    expect_liquidation_price = entry_price - dp \
+        if is_long else entry_price + dp
+
+    # check position's maintenance is same as position returned from state
+    expect = int(expect_liquidation_price)
+    actual = int(state.liquidationPriceEstimate(
+        market, collateral, leverage, is_long))
+    assert expect == approx(actual)
