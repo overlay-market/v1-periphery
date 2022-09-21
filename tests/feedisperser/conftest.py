9fb24b7f88728f201edee80a8166b25400329dc3
realisation <realisation@realisations-MacBook-Pro.local>
2022-09-21 18:11:26 -0400

    updated v1-core version invoked directly in tests

--- tests/feedisperser/conftest.py
+++ tests/feedisperser/conftest.py
@@ -4,7 +4,7 @@ from brownie import Contract, OverlayV1FeeDisperser, web3
 
 @pytest.fixture(scope="module")
 def ovl_v1_core(pm):
-    return pm("overlay-market/v1-core@1.0.0-beta.4")
+    return pm("overlay-market/v1-core@1.0.0-rc.0")
 
 
 @pytest.fixture(scope="module")
